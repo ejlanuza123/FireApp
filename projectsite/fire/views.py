ae8a6812aecc7f6b46d55c1c679ff4bf1e708079
ejlanuza0123 <ejlanuza0123@gmail.com>
2025-04-10 09:48:02 +0800

    Added files

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,25 +1,63 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from fire.models import Locations, Incident, FireStation
+from fire.models import Incident, FireStation, Locations
 
-    #return JsonResponse(result)
 class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
-    
 
-def map_station(request):
-    fireStations = FireStation.objects.values('name', 'latitude', 'longitude')
-
-    for fs in fireStations:
-        fs['latitude'] = float(fs['latitude'])
-        fs['longitude'] = float(fs['longitude'])
+def fire_map(request):
+    # Get all unique cities from incidents
+    cities = Locations.objects.values_list('city', flat=True).distinct()
+    selected_city = request.GET.get('city')
     
-    fireStations_list = list(fireStations)
+    # Get all incidents with related location data
+    incidents = Incident.objects.select_related('location').all()
+    
+    if selected_city:
+        incidents = incidents.filter(location__city=selected_city)
+    
+    # Prepare data for map
+    incidents_data = []
+    for incident in incidents:
+        incidents_data.append({
+            'id': incident.id,
+            'severity': incident.severity_level,
+            'date': incident.date_time.strftime('%Y-%m-%d') if incident.date_time else '',
+            'description': incident.description,
+            'latitude': float(incident.location.latitude),
+            'longitude': float(incident.location.longitude),
+            'address': incident.location.address,
+            'city': incident.location.city
+        })
+    
+    # Get fire stations for reference
+    fire_stations = FireStation.objects.values('name', 'latitude', 'longitude', 'city')
+    stations_data = [{
+        'name': fs['name'],
+        'latitude': float(fs['latitude']),
+        'longitude': float(fs['longitude']),
+        'city': fs['city']
+    } for fs in fire_stations if fs['latitude'] and fs['longitude']]
 
     context = {
-        'fireStations': fireStations_list,
+        'incidents': incidents_data,
+        'stations': stations_data,
+        'cities': cities,
+        'selected_city': selected_city
     }
+    return render(request, 'fire_map.html', context)
 
+def map_station(request):
+    fireStations = FireStation.objects.values('name', 'latitude', 'longitude', 'city')
+    
+    for fs in fireStations:
+        if fs['latitude'] and fs['longitude']:
+            fs['latitude'] = float(fs['latitude'])
+            fs['longitude'] = float(fs['longitude'])
+    
+    context = {
+        'fireStations': [fs for fs in fireStations if 'latitude' in fs],
+    }
     return render(request, 'map_station.html', context)
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
-from fire.views import HomePageView
-from fire import views
+from fire.views import HomePageView, map_station, fire_map
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('stations', views.map_station, name='map-station'),
-
+    path('stations/', map_station, name='map-station'),
+    path('fire-map/', fire_map, name='fire-map'),
 ]
